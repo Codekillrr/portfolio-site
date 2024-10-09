d9e3a9c2b4ae6f60b9a7d5dbf9c65f3555bb9cfc
Abdur Rehman <abdurrehman@Abdurs-MacBook-Air.local>
2024-10-09 09:36:40 +0500

    added some responsivness

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
 
       <div className="z-[3] overflow-auto flex md:flex-row flex-col">
 
-        <header className="z-[4] md:fixed bg-transparent flex align-middle justify-center md:justify-end border-white pb-[12em]  p-[8em] px-[6em] w-full md:w-[50%] h-full"> 
+        <header className="z-[4] md:fixed bg-transparent flex align-middle justify-center md:justify-end border-white max-sm:px-[3em] pb-[12em]  p-[8em] px-[6em] w-full md:w-[50%] h-full"> 
           <div className="flex flex-col justify-between">
             <div className="flex flex-col align-middle">
               <div className="text-4xl max-[441px]:text-3xl sm:text-5xl text-center md:text-6xl font-sans font-bold md:text-right text-slate-300 mt-10">
@@ -56,7 +56,7 @@ export default function Home() {
           </div>
         </header>
 
-        <div className="w-[55%] max-md:w-full pointer-events-auto h-full z-[3] ml-auto mt-[11em] relative max-md:pl-[8em] max-sm:pl-[7em] max-sm:px-[5em] px-[7em] max-[441px]:px-[2em]">
+        <div className="w-[55%] max-md:w-full pointer-events-auto h-full z-[3] ml-auto mt-[11em] relative max-md:pl-[8em] max-sm:pl-[5em] max-sm:px-[3em] px-[7em] max-[441px]:px-[2em]">
           <div className="text-slate-300 max-[441px]:text-sm text-lg font-bold font-sans">
             My software engineering career began as a junior analyst, where I
             honed my analytical and problem-solving skills. Currently, 
@@ -73,14 +73,14 @@ export default function Home() {
 
             <div className="my-5 flex flex-col gap-1">
 
-              <h3 className="text-2xl font-sans font-bold text-[#5ba4d8]" >Software Engineering Fellow</h3>
-              <h4 className="text-xl font-sans font-bold text-[#5ba4d8]">Headstarter AI</h4>
-              <p className="text-lg font-sans text-[#5ba4d8]">
+              <h3 className="max-[441px]:text-xl text-2xl font-sans font-bold text-[#5ba4d8]" >Software Engineering Fellow</h3>
+              <h4 className="max-[441px]:text-lg text-xl font-sans font-bold text-[#5ba4d8]">Headstarter AI</h4>
+              <p className="max-[441px]:text-sm text-lg font-sans text-[#5ba4d8]">
                 <em>
                   July 2024 - September 2024
                 </em>
               </p>
-              <p className="text-slate-300 font-bold text-lg font-sans">
+              <p className="text-slate-300 font-bold max-[441px]:text-sm text-lg font-sans">
                 Fellowship at Headstarter AI, 
                 I immersed myself in the dynamic field of artificial intelligence, 
                 gaining hands-on experience with cutting-edge AI technologies. 
@@ -97,14 +97,14 @@ export default function Home() {
               </p>
 
 
-              <h3 className="text-2xl font-sans font-bold text-[#5ba4d8] mt-4">Junior Analyst</h3>
-              <h4 className="text-xl font-sans font-bold text-[#5ba4d8]">Kaizen Tech Solutions</h4>
-              <p className="text-lg font-sans text-[#5ba4d8]">
+              <h3 className="max-[441px]:text-xl text-2xl font-sans font-bold text-[#5ba4d8] mt-4">Junior Analyst</h3>
+              <h4 className="max-[441px]:text-lg text-xl font-sans font-bold text-[#5ba4d8]">Kaizen Tech Solutions</h4>
+              <p className="max-[441px]:text-sm text-lg font-sans text-[#5ba4d8]">
                 <em>
                   March 2024 - July 2024
                 </em>
               </p>
-              <p className="text-slate-300 font-bold text-lg font-sans">
+              <p className="text-slate-300 font-bold max-[441px]:text-sm text-lg font-sans">
                 Used python libraries to construct charts and graphs, some of the
                 libraries I used were, Matplotlib, Seaborn, Plotly, ggplot and Pandas, to
                 offer a range of functionalities that cater to different needs,
@@ -119,42 +119,42 @@ export default function Home() {
             <h2 className="text-3xl font-bold font-sans text-sky-500 mb-4">Projects</h2>
 
             <div className="flex flex-col w-full h-full">
-              <div className="flex flex-row"> 
-                <button className=" flex flex-col p-4 transition-all ease-in-out duration-200 border-slate-900 w-[50%] hover:border hover:p-[0.94rem] rounded-lg">
+              <div className="flex max-sm:flex-col flex-row"> 
+                <button className=" flex flex-col p-4 transition-all ease-in-out duration-200 border-slate-900 max-sm:w-full w-[50%] hover:border hover:p-[0.94rem] rounded-lg">
                   <div className="bg-black w-full h-[160px] rounded-md"></div>
                   <h4 className="text-xl font-sans font-bold text-[#5ba4d8] mt-4">Redis Clone</h4>
-                  <p className="text-slate-300 text-lg font-bold font-sans text-left">
+                  <p className="text-slate-300 max-[441px]:text-sm text-lg font-bold font-sans text-left">
                     A Redis clone built in C++, providing a high-performance, 
                     in-memory data structure store for caching and real-time 
                     applications with efficient data retrieval and storage capabilities.
                   </p>
                 </button>
 
-                <button className="flex flex-col p-4 transition-all ease-in-out duration-200 border-slate-900 w-[50%] hover:border hover:p-[0.94rem] rounded-lg">
+                <button className="flex flex-col p-4 transition-all ease-in-out duration-200 border-slate-900 max-sm:w-full w-[50%] hover:border hover:p-[0.94rem] rounded-lg">
                   <div className="bg-black w-full h-[160px] rounded-md"></div>
                   <h4 className="text-xl font-sans font-bold text-[#5ba4d8] mt-4">AI RAG Customer Support</h4>
-                  <p className="text-slate-300 text-lg font-bold font-sans text-left">
+                  <p className="text-slate-300 max-[441px]:text-sm text-lg font-bold font-sans text-left">
                     Used python libraries to construct charts and graphs, some of the
                     libraries I used were, Matplotlib, Seaborn, Plotly, ggplot and Pandas.
                   </p>
                 </button>
               </div>
 
-              <div className="flex flex-row"> 
-                <button className="flex flex-col p-4 transition-all ease-in-out duration-200 border-slate-900 w-[50%] hover:border hover:p-[0.94rem] rounded-lg">
+              <div className="flex max-sm:flex-col flex-row"> 
+                <button className="flex flex-col p-4 transition-all ease-in-out duration-200 border-slate-900 max-sm:w-full w-[50%] hover:border hover:p-[0.94rem] rounded-lg">
                   <div className="bg-black w-full h-[160px] rounded-md"></div>
                   <h4 className="text-xl font-sans font-bold text-[#5ba4d8] mt-4">OpenAI Inventory System</h4>
-                  <p className="text-slate-300 text-lg font-bold font-sans text-left">
+                  <p className="text-slate-300 max-[441px]:text-sm text-lg font-bold font-sans text-left">
                     Used python libraries to construct charts and graphs, some of the
                     libraries I used were, Matplotlib, Seaborn, Plotly, ggplot and Pandas, to
                     offer a range of functionalities that cater to different needs,
                     from simple plots to interactive.
                   </p>
                 </button>
-                <button className="flex flex-col p-4 transition-all ease-in-out duration-500 border-slate-900 w-[50%] hover:border hover:p-[0.94rem] rounded-lg">
+                <button className="flex flex-col p-4 transition-all ease-in-out duration-500 border-slate-900 max-sm:w-full w-[50%] hover:border hover:p-[0.94rem] rounded-lg">
                   <div className="bg-black w-full h-[160px] rounded-md"></div>
                   <h4 className="text-xl text-left font-sans font-bold text-[#5ba4d8] mt-4">Analytical Budgeting App</h4>
-                  <p className="text-slate-300 text-lg font-bold font-sans text-left">
+                  <p className="text-slate-300 max-[441px]:text-sm text-lg font-bold font-sans text-left">
                     Used python libraries to construct charts and graphs, some of the
                     libraries I used were, Matplotlib, Seaborn, Plotly, ggplot and Pandas, to
                     offer a range of functionalities that cater to different needs.
@@ -162,7 +162,7 @@ export default function Home() {
                 </button>
               </div>
 
-              <button className="m-8 mx-auto w-[40%] text-lg h-10 hover:border border-slate-900 rounded-lg transition-all ease-in-out bg-slate-700/50 hover:bg-slate-700/70 text-slate-300 font-bold font-sans">View Full Portfolio</button>
+              <button className="m-8 mx-auto max-sm:w-[80%] w-[40%] text-lg h-10 hover:border border-slate-900 rounded-lg transition-all ease-in-out bg-slate-700/50 hover:bg-slate-700/70 text-slate-300 font-bold font-sans">View Full Portfolio</button>
             </div>
 
           </div>
@@ -170,21 +170,21 @@ export default function Home() {
           <div className="my-[4em] p-4 rounded-lg bg-slate-700/[0.2] transition-all ease-in-out duration-200 hover:bg-slate-800/[0.3] backdrop-blur-3xl">
             <h2 className="text-3xl font-bold font-sans text-sky-500 mb-4">Lets Work Together</h2>
 
-            <p className="text-slate-300 text-lg font-bold font-sans text-left">
+            <p className="text-slate-300 max-[441px]:text-sm text-lg font-bold font-sans text-left">
               Get in touch with me and lets build something great together, 
               I should respone within two working day at most, so feel free to reach out!
             </p>
 
-            <div className="flex flex-row h-[5rem] w-[70%] ">
+            <div className="flex flex-row h-[5rem] max-sm:w-full w-[70%] ">
               <input placeholder="Name" className="my-4 p-3 rounded-l-lg w-full bg-slate-800/50 focus:outline-none"/>
               <input placeholder="Email" className="my-4  rounded-r-lg w-full bg-slate-800/50 focus:outline-none"/>
             </div>
 
-            <div className="flex min-h-[10rem] w-[70%] ">
+            <div className="flex min-h-[10rem] max-sm:w-full w-[70%] ">
               <textarea placeholder="Message" className="w-full p-3 min-h-[10rem] rounded-lg bg-slate-800/50 focus:outline-none"></textarea>
             </div>
 
-            <button onClick={()=>SetSent(!sent)} className="my-5 w-[30%] h-[3rem] hover:border-2 hover:bg-slate-700/20 border-slate-900 rounded-lg transition-all ease-in-out bg-slate-800/30 text-slate-300 font-bold font-sans">Send</button>
+            <button onClick={()=>SetSent(!sent)} className="my-5 max-sm:w-[75%] max-sm:mx-12 w-[30%] h-[3rem] hover:border-2 hover:bg-slate-700/20 border-slate-900 rounded-lg transition-all ease-in-out bg-slate-800/30 text-slate-300 font-bold font-sans">Send</button>
             {sent && <div className="p-4 rounded-lg text-center text-lg bg-green-500/10 text-green-500 border border-green-500">SENT!</div>}
           </div>
 
