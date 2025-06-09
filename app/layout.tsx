e588fbfeebbdee05b24c81855632060aede3dfb9
Abdur Rehman <abdurrehman@Abdurs-MacBook-Air.local>
2025-06-09 08:44:20 +0500

    added icons  metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,25 @@ export const metadata: Metadata = {
   title: "Abdur Rehman",
   description:
     "Welcome to my personal website, where I showcase my skills, experience, and expertise. Feel free to explore my portfolio, and don’t hesitate to reach out for collaboration or to hire me directly through this platform.",
+  icons: {
+    icon: [
+      "/favicon.ico?v=4",
+      "/favicon-16x16.png?v=4",
+      "/favicon-32x32.png?v=4",
+    ],
+    apple: ["/apple-touch-icon.png?v=4"],
+    other: [
+      {
+        rel: "icon",
+        url: "/android-chrome-192x192.png?v=4",
+      },
+      {
+        rel: "icon",
+        url: "/android-chrome-512x512.png?v=4",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest?v=4",
 };
 
 export default function RootLayout({
